a7a7e0f71c15089f37d43ce171858f6cac114e95
Jithin-sankar <144434435+Jithin-sankar@users.noreply.github.com>
2026-05-05 12:28:52 +0530

    Time: 2 ms (29.31%), Space: 12.4 MB (20.92%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        return len(set(sentence))==26
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+
+        
