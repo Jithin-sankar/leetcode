3e34e212e699aa8098ed6ba024d3578151c629be
Jithin-sankar <144434435+Jithin-sankar@users.noreply.github.com>
2026-05-18 21:20:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.86%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addDigits(self, num):
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
+
+        """
+        :type num: int
+        :rtype: int
+        """
+        
