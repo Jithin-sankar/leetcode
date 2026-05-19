cf20c19cab83a58b2f1f9130aa729e6b54b70a9f
Jithin-sankar <144434435+Jithin-sankar@users.noreply.github.com>
2026-05-19 17:45:42 +0530

    Time: 23 ms (22.46%), Space: 34.2 MB (9.75%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,14 +1,8 @@
 class Solution(object):
     def getCommon(self, nums1, nums2):
-        i, j = 0, 0
-
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] == nums2[j]:
-                return nums1[i]
-
-            elif nums1[i] < nums2[j]:
-                i += 1
-            else:
-                j += 1
-
-        return -1
+        common = set(nums1) & set(nums2)
+        
+        if not common:   
+            return -1
+        else:
+            return min(common)
