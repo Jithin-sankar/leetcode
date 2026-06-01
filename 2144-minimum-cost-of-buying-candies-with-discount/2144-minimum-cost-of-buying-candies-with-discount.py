37fe846168a020bc067c2da565345867e67f90ba
Jithin-sankar <144434435+Jithin-sankar@users.noreply.github.com>
2026-06-01 17:05:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (51.56%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def minimumCost(self, cost):
         cost.sort(reverse=True)
-
         total = 0
-
         for i in range(len(cost)):
             if (i + 1) % 3 != 0:  
                 total += cost[i]
