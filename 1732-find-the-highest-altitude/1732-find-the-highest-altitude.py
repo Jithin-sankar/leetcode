db511f52a6d39bff4efe13bece2a0c686c20019d
Jithin-sankar <144434435+Jithin-sankar@users.noreply.github.com>
2026-06-19 15:46:08 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.06%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def largestAltitude(self, gain):
-        """
-        :type gain: List[int]
-        :rtype: int
-        """
+        
         alt = [0]
         for i in range(1, len(gain)+1):
             alt.append(gain[i-1] + alt[i-1])
