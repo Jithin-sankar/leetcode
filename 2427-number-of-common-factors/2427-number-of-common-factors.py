6f4052f612a4b05d66899714db08437a12169afe
Jithin-sankar <144434435+Jithin-sankar@users.noreply.github.com>
2026-07-23 18:11:26 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.63%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        count = 0
+        limit = min(a, b)
+
+        for i in range(1, limit + 1):
+            if a % i == 0 and b % i == 0:
+                count += 1
+
+        return count
+        
